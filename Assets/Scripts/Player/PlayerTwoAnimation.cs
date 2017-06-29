284ea3809bf77e5bef3faada9e731e66a68750ad
RennyL <renny.lindberg@hotmail.com>
2017-06-29 12:45:19 +0200

    Animations p1 and p2

--- Assets/Scripts/Player/PlayerTwoAnimation.cs
+++ Assets/Scripts/Player/PlayerTwoAnimation.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayersMovement : MonoBehaviour {
+public class PlayerTwoAnimation : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
